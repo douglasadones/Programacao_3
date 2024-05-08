a0a073507ee9950eed6fa97fe74d7af1bec3618a
Douglas A.S.N <contato.douglas.ad@gmail.com>
2024-05-08 07:02:14 -0300

    corrigindo nome do form

--- javaScript/aula.07.05/scripts.js
+++ javaScript/aula.07.05/scripts.js
@@ -2,35 +2,35 @@
 
 // Acessando: document.forms['cadastro']['produto'] ou document.forms.cadastro.produto 
 
+
 window.addEventListener('load', () => {
-    document.forms.students.addEventListener('submit', formSubmit)
+    document.forms.student.addEventListener('submit', formSubmit)
 })
 
-
 const formSubmit = (event) => {
-    let validForm = true;
-    const form = event.target; // Target informa o elemento que está associado ao evento
+    let validForm = true
+    const form = event.target // target informa o elemento que está associado ao evento
 
-    const studentId = form['student-id'];
-
-    if(studentId.value.lenght != 6) {
-        studentId.nextElementSibling.textContent = 'A matrícula deve possuir 6 caracteres';
-        validForm = true;
-    } 
-
-    const studentName = form['student-name'];
-    if(studentName.value.lenght != 10) {
-        studentName.nextElementSibling.textContent = 'O nome deve possuir no mínimo 10 caracteres';
-        validForm = true;
-    } 
-
-    const studentEmail = form['student-email'];
-    if(studentEmail.value.lenght != 0) {
-        studentEmail.nextElementSibling.textContent = 'O email é obrigatório';
-        validForm = true;
+    const studentId = form['student-id']
+    if (studentId.value.length != 6) {
+        studentId.nextElementSibling.textContent = 'A matrícula deve possuir 6 caracteres'
+        validForm = false
     }
 
-    if (validForm) {
-        event.preventDefault();
+    const studentName = form['student-name']
+    if (studentName.value.length < 10) {
+        studentName.nextElementSibling.textContent = 'O nome deve possuir, no mínimo, 10 caracteres'
+        validForm = false
+    }
+
+    const studentEmail = form['student-email']
+    const reEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/  // RegEx
+    if (!reEmail.test(studentEmail.value)) {
+        studentEmail.nextElementSibling.textContent = 'O e-mail inválido'
+        validForm = false
+    }
+
+    if (!validForm) {
+        event.preventDefault()
     }
 }
