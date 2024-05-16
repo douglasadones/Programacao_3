6f60f6c622c07db2b639e82acdafb5e344ab9cba
Douglas A.S.N <contato.douglas.ad@gmail.com>
2024-05-16 10:13:33 -0300

    adicionando aula

--- Ajax/cep/script.js
+++ Ajax/cep/script.js
@@ -1,45 +1,30 @@
-
-
 window.addEventListener("DOMContentLoaded", () => {
-
     const zipcode = document.getElementById('zipcode');
-
     zipcode.addEventListener('keydown', zipCodeKeyDown);
-    zipcode.addEventListener('keyup', zipCodeChanged);
-
+    zipcode.addEventListener('change', zipCodeChanged);
 });
 
 const zipCodeKeyDown = (event) => {
     if ((event.key < '0' || event.key > '9') && event.key > 31) {
         event.preventDefault();
-        return;
     }
 }
 
 const zipCodeChanged = (event) => {
-
-    const zipCode = event.target.value;
-    
-    if (zipCode.leght != 8) {
+    const zipcode = event.target.value;
+    if (zipcode.length != 8) {
         return
     }
 
     const xhr = new XMLHttpRequest();
-    xhr.open('get', `https://viacep.com.br/ws/${zipCode}/json`, true);
+    xhr.open('get', `https://viacep.com.br/ws/${zipcode}/json`, true);
     xhr.responseType = 'json';
     xhr.onload = () => {
-        
         const data = xhr.response;
-        
         document.getElementById('address').value = data.logradouro;
-
         document.getElementById('neighbor').value = data.bairro;
-
-        document.getElementById('city').value = data.cidade;
-
+        document.getElementById('city').value = data.localidade;
         document.getElementById('state').value = data.uf;
-
     };
     xhr.send();
-
 }
